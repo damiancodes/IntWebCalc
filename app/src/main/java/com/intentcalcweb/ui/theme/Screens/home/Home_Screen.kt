8a9885b4d27cddccfac01602d3b5bc55ed1e86fa
damiancodes <damianbwire88@gmail.com>
2025-04-10 08:34:14 +0300

    initial commit

--- app/src/main/java/com/intentcalcweb/ui/theme/Screens/home/Home_Screen.kt
+++ app/src/main/java/com/intentcalcweb/ui/theme/Screens/home/Home_Screen.kt
@@ -1,13 +1,14 @@
 package com.intentcalcweb.ui.theme.Screens.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,122 +17,63 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-//
-//@Composable
-//fun Home_Screen(modifier: Modifier = Modifier) {
-//    Column (
-//        verticalArrangement = Arrangement.Top,
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//        modifier = Modifier
-//            .fillMaxSize()
-//    ){
-//         Text(
-//             text = "Home Screen",
-//             fontSize = 30.sp,
-//            color = Color.Blue,
-//            fontWeight = FontWeight.Bold,
-//            fontFamily = FontFamily.SansSerif,
-//            fontStyle = FontStyle.Italic
-//                )
-//        Spacer(modifier = Modifier.padding(50.dp))
-//        Button(
-//            onClick = { },
-//              modifier = Modifier.padding(300.dp)
-//        ){
-//            Text(
-//                text = "Intent",
-//                color = Color.Red,
-//                fontSize = 30.sp,
-//                fontWeight = FontWeight.Bold,
-//                fontFamily = FontFamily.SansSerif,
-//                fontStyle = FontStyle.Italic
-//            )
-//        }
-//        Button(onClick = { },
-//            modifier = Modifier.padding(300.dp)){
-//            Text(text = "web",
-//                color = Color.Blue,
-//                fontWeight = FontWeight.Bold,
-//                fontFamily = FontFamily.SansSerif,
-//                fontStyle = FontStyle.Italic
-//            )
-//        }
-//
-//    }
-//
-//}
-//@Preview
-//@Composable
-//private fun Homeprev() {
-//    Home_Screen()
-//}
 
 
 @Composable
 fun Home_Screen(modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Top,
+    Column (verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxSize()
-    ) {
-        Text(
-            text = "Home Screen",
+        modifier = Modifier
+            .background(color = Color.LightGray)
+            .fillMaxSize()){
+        Text(text = "Home screen",
             fontSize = 30.sp,
-            color = Color.Red,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.SansSerif,
-            fontStyle = FontStyle.Italic
+            fontFamily = FontFamily.Monospace,
+            fontStyle = FontStyle.Normal,
+            color= Color.Black
         )
-        Spacer(modifier = Modifier.padding(30.dp))
-        Button(
-            onClick = { /* TODO: */ },
-            modifier = Modifier.width(300.dp)
-        ) {
-            Text(
-                text = "Intent",
-                color = Color.Cyan,
+        Spacer(modifier = Modifier.height(50.dp))
+        Button(onClick = { TODO() },
+            modifier = Modifier.width(300.dp)) {
+            Text(text="Calculator",
                 fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.SansSerif,
-                fontStyle = FontStyle.Italic
+                fontFamily = FontFamily.Serif,
+                fontStyle = FontStyle.Italic,
+                color = Color.Cyan
             )
         }
-        Spacer(modifier = Modifier.padding(30.dp))
-        Button(
-            onClick = { /* TODO:  */ },
-            modifier = Modifier.width(300.dp)
-        ) {
-            Text(
-                text = "Web",
-                color = Color.Cyan,
+        Spacer(modifier = Modifier.height(200.dp))
+        Button(onClick = { TODO() },
+            modifier = Modifier.width(300.dp)) {
+            Text(text="Intent",
                 fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.SansSerif,
-                fontStyle = FontStyle.Italic
+                fontFamily = FontFamily.Serif,
+                fontStyle = FontStyle.Italic,
+                color = Color.Cyan
             )
         }
-        Spacer(modifier = Modifier.padding(30.dp))
-        Button(
-            onClick = { /* TODO */ },
-            modifier = Modifier.width(300.dp)
-        ) {
-            Text(
-                text = "Calculator",
-                color = Color.Cyan,
+        Spacer(modifier = Modifier.height(200.dp))
+        Button(onClick = { TODO() },
+            modifier = Modifier.width(300.dp)) {
+            Text(text="Web",
                 fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.SansSerif,
-                fontStyle = FontStyle.Italic
+                fontFamily = FontFamily.Serif,
+                fontStyle = FontStyle.Italic,
+                color = Color.Cyan
             )
         }
+
+
     }
+
 }
+
 @Preview
 @Composable
-private fun Homeprev() {
+private fun Homepreview() {
     Home_Screen()
+
 }
